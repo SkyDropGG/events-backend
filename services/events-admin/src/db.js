b77327c43c9029c8c918b11c166e0dd84d69029e
SkyDrop <figshenbk@yandex.com>
2017-08-10 20:04:45 +0300

    Add port to db config

--- services/events-admin/src/db.js
+++ services/events-admin/src/db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const config = require('./config');
 const sequelize = new Sequelize(config.db.database, config.db.username, config.db.password,
-  {dialect: 'postgres', host: config.db.host});
+  {dialect: 'postgres', host: config.db.host, port: config.db.port});
 
 module.exports.Events = sequelize.define('events', {
   title: Sequelize.STRING,
